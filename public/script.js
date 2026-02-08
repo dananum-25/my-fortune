9e33f605ad12d2602fd7c98f34ca1aaa4b8fb056
dananum-25 <sh2439.kim@gmail.com>
2026-02-08 20:29:55 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -306,7 +306,7 @@ chat.scrollIntoView({ behavior:"smooth", block:"start" });
 
 await wait(400);
 await typeHTML(chat, readingHTML, 16);
-
+}
 /* =====================================================
 8. FIRE: REORDER → BIG
 ===================================================== */
